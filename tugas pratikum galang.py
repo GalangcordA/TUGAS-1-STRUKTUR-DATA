d170e386e4ea0753b1359bbe461278c9eeda699f
GalangcordA <galangkanedi99@gmail.com>
2025-04-21 12:46:33 +0800

    tugas function

---
+++ tugas pratikum galang.py
@@ -0,0 +1,28 @@
+def sapa():
+    print("Halo, selamat datang!")
+
+sapa()  # Memanggil fungsi
+sapa()
+sapa()
+
+# function dengan 1 parameter
+def sapa_nama(nama):
+    print(f"Halo, {Galang kanedi}!")
+
+# Pemanggilan Function
+sapa_nama("Galang kanedi")
+
+def function_name(param1, param2=default_value2, param3=default_value3)
+def student(firstname, lastname ='Mark', standard ='Fifth'):
+    print(firstname, lastname, 'studies in', standard, 'Standard')
+
+    # *args untuk argumen bervariasi
+def myFun(*argv):
+    for arg in argv:
+        print(arg)
+
+myFun('Hello', 'Selamat Datang', 'Di', 'PTI UNDIKMA')
+
+
+
+
